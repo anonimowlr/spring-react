9326323807dedfcb5587cec6bbad14d3e01be832
JOCIMAR <jocimarwalter@bb.com.br>
2021-09-08 18:58:07 -0300

    graficos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import Barchart from "components/BarChart";
+import Datatable from "components/DataTable";
+import DonutsChart from "components/DonutsChart";
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
 
@@ -6,7 +9,24 @@ function App() {
     <>
       <Navbar></Navbar>
       <div className="container">
-        <h1 className="text-primary">Hello word</h1>
+        <h1 className="text-primary py-3">DashBord Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
+            <Barchart></Barchart>
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
+            <DonutsChart/>
+          </div>
+
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+
+        </div>
+        <Datatable></Datatable>
       </div>
       <Footer></Footer>
     </>
